79b0e4f99124833506618006b89840238aa8f726
captivating-mapping-7374 <mxharris1094@yahoo.com>
2019-12-10 05:13:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,7 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+name = "Josh"
 def introduction(name)
   print "Hi, my name is #{name}"
 end
@@ -15,7 +16,7 @@ def introduction_with_language_optional(name,language = "Ruby")
   print "Hi,my name is #{name} and I am learning to program in #{language}."
 end
 
-name = "Josh"
+
 introduction(name)
 
 introduction_with_language(name,language)
