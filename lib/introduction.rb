094ce14ef27b7252b173e1f20eb710ffd77d0a48
captivating-mapping-7374 <mxharris1094@yahoo.com>
2019-12-10 05:27:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,11 +12,12 @@ def introduction(name)
 end
 
 def introduction_with_language(name,language)
-  print  "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts  "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-name = "Edwin"
+
+
 def introduction_with_language_optional(name,language = "Ruby")
-  print "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction(name)
